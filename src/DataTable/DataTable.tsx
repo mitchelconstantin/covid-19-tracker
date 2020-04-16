c3b2c998f5e26950329e27a9dec956eb5fcd6da2
Mitchel Constantin <mitchconstantin@gmail.com>
2020-04-16 11:37:39 -0500

    show all data for selected countries in table

--- src/DataTable/DataTable.tsx
+++ src/DataTable/DataTable.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
+import MUIDataTable from 'mui-datatables';
 import { CountryDictionary, FormState } from '../shared/Types';
 
 interface Props {
   formState: FormState;
   data: CountryDictionary;
 }
+
 //todo move this method up so the graph can use it
 //probably can be in a behaivor on the app itself
 const getSelectedCountriesData = (
   data: CountryDictionary,
-  countries: string[]
+  formState: FormState
 ) => {
-  return countries.map((country) => ({ [country]: data[country] }));
+  // const selectedCountries = countries.map((country) => ({
+  //   [country]: data[country],
+  // }));
+
+  //todo consider another way to make an enum of types
+  const columns = ['country', 'date', 'confirmed', 'deaths', 'recovered'];
+
+  //todo add types for rows
+  //@ts-ignore
+  const rows = [];
+
+  formState.selectedCountries.forEach((country) => {
+    data[country].forEach((dataPoint) => {
+      rows.push([
+        country,
+        dataPoint.date,
+        dataPoint.confirmed,
+        dataPoint.deaths,
+        dataPoint.recovered,
+      ]);
+    });
+  });
+  //@ts-ignore
+  return { columns, rows };
 };
 
 export const DataTable = ({ formState, data }: Props) => {
-  const rowsToShow = getSelectedCountriesData(
-    data,
-    formState.selectedCountries
-  );
+  const { columns, rows } = getSelectedCountriesData(data, formState);
 
-  console.log('rows to show,', rowsToShow);
-  return <>{formState.selectedCountries.map((country) => country)}</>;
+  return (
+      <MUIDataTable title={'Raw data'} data={rows} columns={columns} />
+  );
 };
