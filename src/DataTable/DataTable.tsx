c74814025cadea4196ae60cca7cb2ebc7f391794
Mitchel Constantin <mitchconstantin@gmail.com>
2020-04-17 09:31:42 -0500

    show correct dates for table headers

--- src/DataTable/DataTable.tsx
+++ src/DataTable/DataTable.tsx
@@ -6,26 +6,13 @@ interface Props {
   data: CountryDictionary;
 }
 
-// const formatData = (data: CountryDictionary) => {
-//   //todo consider another way to make an enum of types
-//   // columns should be ['country', then date, date, date, date etc.]
-//   const columns = ['country', 'date', 'confirmed', 'deaths', 'recovered'];
-//   const dataSets = Object.entries(data).map(([country, dataPoints]) => {
-//     return dataPoints.map((dataPoint) => [
-//       country,
-//       dataPoint.date,
-//       dataPoint.confirmed,
-//       dataPoint.deaths,
-//       dataPoint.recovered,
-//     ]);
-//   });
-
-//   //combine sub arrays into one large array
-//   const rows = dataSets.flat();
-//   return { columns, rows };
-// };
 const formatData = (data: CountryDictionary) => {
-  const columns = ['country', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const columns = ['country'];
+  for (let key in data) {
+    const columnData = data[key].map(({ date }) => date);
+    columns.push(...columnData);
+    break;
+  };
   //@ts-ignore
   const rows = Object.entries(data).map(([country, dataPoints]) => {
     const rowData = dataPoints.map((dataPoint) => dataPoint.confirmed);
