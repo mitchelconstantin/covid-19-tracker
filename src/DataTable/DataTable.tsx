4706536ca4101a727916895a18875dbc39fabd4e
Mitchel Constantin <mitchconstantin@gmail.com>
2020-04-17 09:20:56 -0500

    use one row per country to show confirmed cases in graph

--- src/DataTable/DataTable.tsx
+++ src/DataTable/DataTable.tsx
@@ -6,27 +6,43 @@ interface Props {
   data: CountryDictionary;
 }
 
-const formatData = (data: CountryDictionary) => {
-  //todo consider another way to make an enum of types
-  const columns = ['country', 'date', 'confirmed', 'deaths', 'recovered'];
-  const dataSets = Object.entries(data).map(([country, dataPoints]) => {
-    return dataPoints.map((dataPoint) => [
-      country,
-      dataPoint.date,
-      dataPoint.confirmed,
-      dataPoint.deaths,
-      dataPoint.recovered,
-    ]);
-  });
-  //todo consider keeping seperate and having distinct graphs for each country
+// const formatData = (data: CountryDictionary) => {
+//   //todo consider another way to make an enum of types
+//   // columns should be ['country', then date, date, date, date etc.]
+//   const columns = ['country', 'date', 'confirmed', 'deaths', 'recovered'];
+//   const dataSets = Object.entries(data).map(([country, dataPoints]) => {
+//     return dataPoints.map((dataPoint) => [
+//       country,
+//       dataPoint.date,
+//       dataPoint.confirmed,
+//       dataPoint.deaths,
+//       dataPoint.recovered,
+//     ]);
+//   });
 
-  //combine sub arrays into one large array
-  const rows = dataSets.flat();
+//   //combine sub arrays into one large array
+//   const rows = dataSets.flat();
+//   return { columns, rows };
+// };
+const formatData = (data: CountryDictionary) => {
+  const columns = ['country', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  //@ts-ignore
+  const rows = Object.entries(data).map(([country, dataPoints]) => {
+    const rowData = dataPoints.map((dataPoint) => dataPoint.confirmed);
+    return [country, ...rowData];
+  });
+
   return { columns, rows };
 };
 //todo see render errors in console and fix
 export const DataTable = ({ data }: Props) => {
   const { columns, rows } = formatData(data);
 
-  return <MUIDataTable title={'Raw data'} data={rows} columns={columns} />;
+  return (
+    <MUIDataTable
+      title={'Confirmed Cases by date'}
+      data={rows}
+      columns={columns}
+    />
+  );
 };
